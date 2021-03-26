0d2672e8fbeebb34b4091be5052870cebeb224d3
Swadesh Behera <itswadesh@gmail.com>
2021-03-26 13:51:29 +0530

    improve addToCart

--- src/utils/cart.ts
+++ src/utils/cart.ts
@@ -29,14 +29,21 @@ export const clearCart = async (req: Request) => {
 }
 
 //cart merge function
-export const merge = async (req: Request, user: String) => {
+export const merge = async (req: Request) => {
+  const user = req.session.userId
   if (!user) return
   let dbCart = await Cart.findOne({ user })
   if (dbCart) {
     let items = dbCart.items
     if (items.length > 0) {
       const promises = items.map(async (i) => {
-        await addToCart(req, { pid: i.product._id, qty: i.qty })
+        await addToCart(req, {
+          pid: i.pid,
+          vid: i.vid,
+          options: i.options,
+          vendor: i.vendor,
+          qty: i.qty,
+        })
       })
       await Promise.all(promises)
     }
@@ -50,8 +57,26 @@ export const merge = async (req: Request, user: String) => {
 
 export const addToCart = async (
   req: Request,
-  { pid, vid, qty, replace }: any
+  { pid, vid, qty, options, replace }: any
 ) => {
+  vid = pid
+  if (options != undefined) options = JSON.parse(options)
+  else options = []
+  // Decided to stick to string version because its easy for both front and backend
+  // if (options) {
+  //   let optn: any
+  //   optn = options
+  //   if (optn) optn = Object.entries(optn)
+  //   else optn = []
+  //   options = []
+  //   for (let o of optn) {
+  //     options.push({ name: o[0], val: o[1] })
+  //   }
+  // } else {
+  //   options = []
+  // }
+
+  // console.log('zzzzzzzzzzzzzzzzzzzzzzzzzzz', options)
   // Req required for accessing session
   //   console.log("this session id " + req.sessionID);
   let product: any = {}
@@ -71,55 +96,85 @@ export const addToCart = async (
 
   if (+qty === 0) {
     // When specifically told to remove from cart
-    items = removeFromCartSession(items, pid, vid)
+    items = removeFromCartSession(items, pid, vid, options)
     await calculateSummary(req)
     // saveMyCart(req.session.cart); // email inside cart is mandatory
     return req.session.cart
   }
   try {
     // Required for stock verification
-    product = await Product.findById(pid).select(
-      'name slug img price time vendor stock'
-    )
-    vid = 0
+    product = await Product.findById(pid)
+      .select('name slug img price time vendor stock')
+      .populate('vendor')
+    // vid = 0
     if (!product) {
-      items = removeFromCartSession(items, pid, vid)
+      items = removeFromCartSession(items, pid, vid, options)
       const code = req.session.cart.discount && req.session.cart.discount.code
       await calculateSummary(req, code)
       return req.session.cart
     }
   } catch (e) {
-    items = removeFromCartSession(items, pid, vid)
+    items = removeFromCartSession(items, pid, vid, options)
     throw new UserInputError(e.toString())
   }
   if (!product) throw new UserInputError('Product not found')
-  const { name, slug, img, price, time } = product
-  // if (!_id || !vendor || !vendor.info) throw new UserInputError('Restaurant info missing')
+  const { name, slug, img, price, time, vendor } = product
+  // if (!_id || !vendor || !vendor.info) throw new UserInputError('store info missing')
   // if (
   //   req.session.cart.vendor &&
   //   req.session.cart.vendor._id != vendor._id &&
   //   items.length > 0
   // )
   //   throw new UserInputError(
-  //     `Your cart contain dishes from ${req.session.cart.vendor.info.restaurant}. Do you wish to Cart cart and add dishes from ${vendor.info.restaurant}?`
-  //   )
-  const record = items.find((p: CartItemDocument) => p.pid === pid) || {}
+  //     `Your cart contain dishes from ${req.session.cart.vendor.info.store}. Do you wish to Cart cart and add dishes from ${vendor.info.store}?`
+  //   )ObjectId("5ea4351c880b0b23119f4c1d")
+  const record =
+    items.find((p: CartItemDocument) => {
+      // console.log('zzzzzzzzzzzzzzzzzzzzzzzzzzz', p.vid, vid)
+      const pv = p.pid === pid && p.vid == vid
+      let o: any = JSON.parse(p.options || '{}')
+      let matched = true
+      for (let k in o) {
+        // console.log('oooooooooo', k, o[k], options[k])
+        if (o[k] !== options[k]) {
+          matched = false
+          continue
+        }
+      }
+      return pv && matched
+    }) || {}
 
+  // console.log('Record............', req.session.cart.items)
   if (+product.stock < +record.qty + qty && +qty > 0)
     throw new UserInputError('Not enough stock')
 
   if (record.qty) {
-    console.log('Already in cart', pid)
+    // console.log('if record already exist', pid, vid, qty)
+    // console.log('Not in cart', options)
     // If the product is already there in cart increase qty
+    // if (+record.qty < 2) qty = 1
     record.qty = +record.qty + +qty
     if (+record.qty < 1) {
       // When stock not enough remove it from cart
-      items = removeFromCartSession(items, pid, vid)
+      items = removeFromCartSession(items, pid, vid, options)
     }
-  } else {
-    console.log('Not in cart', pid)
-    items.push({ pid, name, slug, img, price, qty, time })
+  } else if (qty > 0) {
+    // console.log('First time ', pid, vid, qty)
+    items.push({
+      pid,
+      vid,
+      options: JSON.stringify(options),
+      vendor,
+      name,
+      slug,
+      img,
+      price,
+      qty,
+      time,
+    })
   }
+  // console.log('Last Step of addToCart', req.session.cart.items)
+  // req.session.cart.items = []
   // req.session.cart.vendor = vendor
   const code = req.session.cart.discount && req.session.cart.discount.code
   await calculateSummary(req, code)
@@ -130,10 +185,11 @@ export const addToCart = async (
 export const removeFromCartSession = async (
   items: [CartItemDocument],
   pid: string,
-  vid: string
+  vid: string,
+  options: string
 ) => {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].pid === pid) {
+    if (items[i].pid === pid && items[i].vid === vid) {
       items.splice(i, 1)
     }
   }
@@ -210,7 +266,7 @@ export const applyDiscount = (
 ): number => {
   let amount: number = 0
   if (!subtotal || subtotal < 1 || subtotal < minimumCartValue) return 0
-  if (type == 'Percent') amount = subtotal * (value / 100)
+  if (type == 'Percent') amount = Math.floor(subtotal * (value / 100))
   else if (type == 'Fixed') amount = value
   if (amount > maxAmount) amount = maxAmount
   return amount
@@ -281,6 +337,7 @@ export const calculateSummary = async (req: Request, code?: string) => {
   // Other validations moved to separate function named validateCart because when cart is cleared, validate cart will throw error of minimumordervalue + cart is empty
   const { session } = req
   const { cart, userId, id } = session
+  if (!cart) throw new UserInputError('No items in cart')
   let { items } = cart
   cart.qty = getTotalQty(items)
   let subtotal = (cart.subtotal = await getSubTotal(items))
